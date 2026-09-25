f950746cd516da439ab8b63c24037a8ecca0f824
marlon978-create <mptejada2001@gmail.com>
2026-09-24 21:04:01 -0400

    Added mobile menu

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+if (menuToggle){
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("mobile-open");
+    });
+}
